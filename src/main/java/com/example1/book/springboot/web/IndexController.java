15ecc9e4a464514fe1aa4a71f616203ba3b4ee9d
Nam <stellis001@gmail.com>
2020-11-22 17:14:31 +0900

    chapter 5 complete

--- src/main/java/com/example1/book/springboot/web/IndexController.java
+++ src/main/java/com/example1/book/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.example1.book.springboot.web;
 
+import com.example1.book.springboot.config.auth.LoginUser;
+import com.example1.book.springboot.config.auth.dto.SessionUser;
+import com.example1.book.springboot.domain.user.User;
 import com.example1.book.springboot.service.posts.PostsService;
 import com.example1.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -12,16 +15,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts", postsService.findAllDesc());//메인화면에 글 목록 보여주기
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("name", user.getName());
+        }
+
         return "index";
     }
 
@@ -37,4 +48,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
